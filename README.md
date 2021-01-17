45d9edc4a8985c2efda243f7fea571e0c7885bae
teachwiki <35622257+teachwiki@users.noreply.github.com>
2021-01-17 03:54:49 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AwwApp
+# Shared-Board-Using-AwwApp
+A collaborative board built by using AwwApp's API v2.
---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Aww App Api
+var aww = new AwwBoard('#aww-wrapper', {});
+
+// Footer
+var today = new Date;
+document.getElementById('year').innerHTML = today.getFullYear();
